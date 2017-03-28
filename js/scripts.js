7395dade00560ad54718cbc6383c51a0b53f5ebd
Asia Kane and Jennifer Kinsey <jennifer@>
2017-03-28 08:52:37 -0700

    update js script to use for loop instead of .forEach

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,26 @@ $(function(){
   $("#formOne").submit(function(event){
     var blanks = ["person1","person2","animal","exclamation","verb","noun"];
 
-    blanks.forEach(function(blank) {
-      var userInput = $("#" + blank).val();
-      $("." + blank).text(userInput);
-    });
+// Code using for loop
+    for (var i = 0; i < blanks.length; i++){
+      var userInput = $("#" + blanks[i]).val();
+      $("."+ blanks[i]).text(userInput);
+    }
+
+// Code using .forEach loop
+    // blanks.forEach(function(blank) {
+    //   var userInput = $("#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
 
 
+// Code without looping
     // var person1Input = $("input#person1").val();
     // var person2Input = $("input#person2").val();
     // var animalInput = $("input#animal").val();
     // var exclamationInput = $("input#exclamation").val();
     // var verbInput = $("input#verb").val();
     // var nounInput = $("input#noun").val();
-    //
-    //
-    //
     //
     // $(".person1").append(person1Input);
     // $(".person2").append(person2Input);
@@ -29,9 +34,4 @@ $(function(){
 
     event.preventDefault();
   });
-
-
-
-
-
 });
